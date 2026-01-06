2ae7a2cdad06dfe7faeff6faa95436f81e661b4f
Marcel Ramos <marceldavid@hotmail.com>
2026-01-06 13:05:05 -0500

    Set global styles with S.C. library

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 //styled component
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-  width: 150px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -33,12 +17,16 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis Hotel</H1>
-      <Input type="number" placeholder="Number of guest"></Input>
-      <Button onClick>Check in</Button>
-      <Button onClick>Check out</Button>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis Hotel</H1>
+        <Button onClick>Check in</Button>
+        <Button onClick>Check out</Button>
+        <Input type="number" placeholder="Number of guest"></Input>
+        <Input type="number" placeholder="Number of guest"></Input>
+      </StyledApp>
+    </>
   );
 }
 
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,22 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shaddow-sm);
+  cursor: pointer;
+  /* width: 150px; */
+  /** & = Selects the auto generated class of the component*/
+  &:hover {
+    background-color: var(--color-brand-800);
+  }
+`;
+
+export default Button;

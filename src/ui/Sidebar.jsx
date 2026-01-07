fe3ddc9b0c55fb0278ef2dc6341058d47817d454
Marcel Ramos <marceldavid@hotmail.com>
2026-01-07 16:15:48 -0500

    Build Siedbar and MainNav components

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
 
 const StyledSideBar = styled.aside`
   background-color: var(--color-grey-0);
@@ -6,10 +8,17 @@ const StyledSideBar = styled.aside`
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/ -1;
+  display: flex;
+  flex-direction: column;
 `;
 
 function Sidebar() {
-  return <StyledSideBar>SIDEBAR</StyledSideBar>;
+  return (
+    <StyledSideBar>
+      <Logo />
+      <MainNav />
+    </StyledSideBar>
+  );
 }
 
 export default Sidebar;

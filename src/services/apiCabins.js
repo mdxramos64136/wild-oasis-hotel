9ad7ab9bccc8f300b7d278e817856d6f1a29f8e7
Marcel Ramos <marceldavid@hotmail.com>
2026-01-07 20:48:42 -0500

    Set Supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,15 @@
+import supabase from "./supabase";
+
+// return all fields of cabins table
+// from = name of the table
+// select = fields
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://kaqpopgsrqnpsioynwiv.supabase.co";
+const supabaseKey = "sb_publishable_NMeuAjghi0-QctXcgetUzQ_30Iz0dTw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

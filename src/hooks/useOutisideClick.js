2ba3498d3b5798843b965698c3434e0d1224c637
Marcel Ramos <marceldavid@hotmail.com>
2026-02-12 10:19:16 -0500

    Handle modal closing when clicking outside

---
+++ src/hooks/useOutisideClick.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handleClose) {
+  const ref = useRef();
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handleClose();
+        }
+      }
+      document.addEventListener("click", handleClick, true);
+
+      //remove event listener after component unmounts
+      return () => document.removeEventListener("click", handleClick);
+    },
+    [handleClose],
+  );
+  return ref;
+}

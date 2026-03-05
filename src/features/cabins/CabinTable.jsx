e6a8fd28f24cb6a1e3e10db5dbd9269de06b928d
Marcel Ramos <marceldavid@hotmail.com>
2026-03-05 16:57:24 -0500

    Implement cabin filtering features

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,31 +1,29 @@
-import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 // import Cabins from "../../pages/Cabins";
 import CabinRow from "./CabinRow";
 import { useFetchCabins } from "./useFetchCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr; //grid with 6 columns
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 /************************** COMPONENT **************************/
 function CabinTable() {
   const { isLoading, cabins } = useFetchCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+  console.log(filterValue);
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   /////////////
   return (
@@ -42,7 +40,7 @@ function CabinTable() {
 
         {/* Usin Render Pattern */}
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +34,30 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  //verify wich button is selected and then set "active" style
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+  //
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          active={option.value === currentFilter}
+          key={option.value}
+          onClick={() => handleClick(option.value)}>
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
